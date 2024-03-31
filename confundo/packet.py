735accb0ea6338a6973a0c0ee83fac55eacd52d6
remyy1999 <112822636+remyy1999@users.noreply.github.com>
2024-03-31 13:41:05 -0400

    Update packet.py

--- confundo/packet.py
+++ confundo/packet.py
@@ -1,21 +1,32 @@
-# -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
-# Copyright 2019 Alex Afanasyev
-#
+import struct
 
-from .header import Header
+class Packet:
+    HEADER_FORMAT = "!LLHHBBB"  # Format string for struct.pack
 
-class Packet(Header):
-    '''Abstraction to handle the whole Confundo packet (e.g., with payload, if present)'''
-
-    def __init__(self, payload=b"", isDup=False, **kwargs):
-        super(Packet, self).__init__(**kwargs)
+    def __init__(self, seqNum=0, ackNum=0, connId=0, isSyn=False, isFin=False, isAck=False, payload=b""):
+        self.seqNum = seqNum
+        self.ackNum = ackNum
+        self.connId = connId
+        self.isSyn = isSyn
+        self.isFin = isFin
+        self.isAck = isAck
         self.payload = payload
-        self.isDup = isDup # only for printing flags
-
-    def decode(self, fullPacket):
-        super(Packet, self).decode(fullPacket[0:12])
-        self.payload = fullPacket[12:]
-        return self
 
     def encode(self):
-        return super(Packet, self).encode() + self.payload
+        header = struct.pack(Packet.HEADER_FORMAT,
+                             self.seqNum,
+                             self.ackNum,
+                             self.connId,
+                             0,  # Not Used (13 bits)
+                             int(self.isAck) << 2 | int(self.isSyn) << 1 | int(self.isFin))  # A|S|F bits
+        return header + self.payload
+
+    @classmethod
+    def decode(cls, data):
+        seqNum, ackNum, connId, _, flags = struct.unpack(Packet.HEADER_FORMAT, data[:12])
+        isAck = bool(flags & 0b100)
+        isSyn = bool(flags & 0b010)
+        isFin = bool(flags & 0b001)
+        payload = data[12:]
+        return cls(seqNum, ackNum, connId, isSyn, isFin, isAck, payload)
+

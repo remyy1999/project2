d3612063aa711958e6bd50e303d4c827c58295af
remyy1999 <112822636+remyy1999@users.noreply.github.com>
2024-03-31 13:04:46 -0400

    Update common.py

--- confundo/common.py
+++ confundo/common.py
@@ -9,3 +9,8 @@ SYN_WAIT_TIME = 10.0
 INIT_SSTHRESH = 10000
 
 GLOBAL_TIMEOUT = 10.0
+
+# Parameters for congestion control
+MAX_WINDOW_SIZE = 10000
+INIT_CWND_SIZE = 10
+CC_TIMEOUT = 1.0  # Timeout value for congestion control
